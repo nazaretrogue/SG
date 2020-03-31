93d1af5a33b7f0fda3ad8c0917012e10114e8733
Nazaret <nazaret.ro.gue@gmail.com>
2020-03-31 16:25:20 +0200

    Añadido escenario de lucha

---
+++ Practicas/BLEACH RPG/escenario/escenario.js
@@ -0,0 +1,33 @@
+class Escenario extends THREE.Object3D {
+  constructor(gui,titleGui) {
+    super();
+
+    this.createGUI(gui,titleGui);
+
+    var modelo_loader = new THREE.OBJLoader();
+    var material_loader = new THREE.MTLLoader();
+
+    var that = this;
+
+    material_loader.load('../models/escenario/escenario.mtl',
+                         function(material){
+                           modelo_loader.setMaterials(material);
+                           material.side = THREE.DoubleSide;
+                           modelo_loader.load('../models/escenario/escenario.obj',
+                                              function(obj){
+                                                var modelo = obj;
+                                                that.add(modelo);
+                                              },
+                                              null, null);});
+    this.scale.set(10, 10, 10);
+    //this.rotation.x = -Math.PI/2;
+  }
+
+  createGUI(gui, titleGui){
+    //todo
+  }
+
+  update(){
+    //todo
+  }
+}
