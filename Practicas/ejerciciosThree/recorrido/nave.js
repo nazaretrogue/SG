829015f000124a8cccd68e12dcc90c8473800bf4
Nazaret <nazaret.ro.gue@gmail.com>
2020-04-06 21:54:25 +0200

    Añadida animación de la nave

--- Practicas/ejerciciosThree/recorrido/nave.js
+++ Practicas/ejerciciosThree/recorrido/nave.js
@@ -7,35 +7,52 @@ class Nave extends THREE.Object3D{
     var textura = new THREE.TextureLoader().load('../imgs/textura-ajedrezada.jpg');
     var material = new THREE.MeshPhongMaterial({map: textura});
 
-    var recorrido = [new THREE.Vector3(-6, 0, 6), new THREE.Vector3(-5, 5, 5),
-                     new THREE.Vector3(0, 0, 0), new THREE.Vector3(5, -2, 5),
-	                   new THREE.Vector3(10, 0, 10), new THREE.Vector3(7, 1, 1),
-                     new THREE.Vector3(5, 0, 0), new THREE.Vector3(-8, -4, 6),
-                     new THREE.Vector3(-6, 0, 6)];
+    var recorrido = [new THREE.Vector3(0, 0, 0), new THREE.Vector3(10, -3, -5),
+                     new THREE.Vector3(25, 0, 0), new THREE.Vector3(10, 3, 5),
+                     new THREE.Vector3(0, 0, 0), new THREE.Vector3(-10, -3, -5),
+                     new THREE.Vector3(-25, 0, 0), new THREE.Vector3(-10, 3, 5),
+                     new THREE.Vector3(0, 0, 0)];
 
-    var curva = new THREE.CatmullRomCurve3(recorrido);
+    this.curva = new THREE.CatmullRomCurve3(recorrido);
 
-    var puntos = curva.getPoints(50);
+    var puntos = this.curva.getPoints(50);
     var geometry = new THREE.BufferGeometry().setFromPoints(puntos);
-    var material_8 = new THREE.LineBasicMaterial({color: 0x0000 });
+    var material_8 = new THREE.LineBasicMaterial({color: 0x000000});
 
     var spline = new THREE.Line(geometry, material_8);
+    this.nave_girada = new THREE.Mesh(geometria, material);
+    this.nave = new THREE.Object3D();
 
-    this.nave = new THREE.Mesh(geometria, material);
+    this.nave_girada.rotation.x = Math.PI/2;
 
-    this.nave.position.y = 2;
-
+    this.nave.add(this.nave_girada);
     this.add(this.nave);
     this.add(spline);
   }
 
   createGUI(gui, titleGui){
     this.guiControls = new function(){
-
+      this.bucle_izdo = true;
     }
   }
 
   update(){
+    var tiempo = Date.now();
+    var tiempo_recorrido = 4000;
+    var t = (tiempo%tiempo_recorrido)/tiempo_recorrido;
+    var pos = this.curva.getPointAt(t);
+    var tangente = this.curva.getTangentAt(t);
 
+    if(this.guiControls.bucle_izdo){
+      tiempo_recorrido *= 2;
+      t = (tiempo%tiempo_recorrido)/tiempo_recorrido;
+      pos = this.curva.getPointAt(t);
+      tangente = this.curva.getTangentAt(t);
+    }
+
+    this.nave.position.copy(pos);
+
+    pos.add(tangente);
+    this.nave.lookAt(pos);
   }
 }
